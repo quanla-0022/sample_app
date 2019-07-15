03d402d428a1cdb80bc8dd0163654013000b5d09
Le Anh Quan <le.anh.quan@sun-asterisk.com>
2019-07-15 15:18:42 +0700

    Finish chapter 14

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module SampleApp
     config.i18n.available_locales = [:en,:vi]
     config.i18n.default_locale = :en
     config.middleware.use I18n::JS::Middleware
+    config.action_view.embed_authenticity_token_in_remote_forms = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
